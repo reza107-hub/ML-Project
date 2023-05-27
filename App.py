b1decc9b90c75da71a002faad45489ab8ab39340
reza107-hub <rashidaminur104@gmail.com>
2023-05-27 13:25:42 +0600

    books ui fetched from app.py and showed in ui

--- App.py
+++ App.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template
 import pickle
 
 
-popular_df = pickle.load(open('popular.pkl','rb'))
+popular_df = pickle.load(open('popular.pkl', 'rb'))
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html',
-                           book_name = list(popular_df['Book-Title'].values),
+                           book_name=list(popular_df['Book-Title'].values),
                            author=list(popular_df['Book-Author'].values),
                            image=list(popular_df['Image-URL-M'].values),
                            votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
+                           rating=[round(rating, 1)
+                                   for rating in popular_df['avg_rating'].values]
                            )
 
 
